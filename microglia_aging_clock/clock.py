0bcb90861fca08a6f2e980777b6e5ae5ae3a6bc1
Luvna Dhawka <ldhawka@unc.edu>
2024-09-08 20:38:02 -0400

    updating comment

--- microglia_aging_clock/clock.py
+++ microglia_aging_clock/clock.py
@@ -3,8 +3,9 @@ import os
 import anndata
 import scanpy as sc
 import numpy as np
+import math # need to import math
 
-def frequency_feats(data = None, meta_label = 'meta_label'):
+def frequency_feats(data=None, meta_label='meta_label'):
     """
     Computes frequency features (proportion of cells assigned to a metacluster within a sample)
 
@@ -19,14 +20,15 @@ def frequency_feats(data = None, meta_label = 'meta_label'):
     freq_feats: pandas.DataFrame
         dataframe containing frequency features for each sample
     """
+
     num = data.groupby(['sample_id', meta_label]).size().unstack().fillna(0)
     denom = num.sum(1)
-    freq_feats = num.divide(denom, axis = 0)
+    freq_feats = num.divide(denom, axis=0)
     freq_feats.columns = ['freq_{}'.format(i) for i in freq_feats.columns]
-
     return freq_feats
 
-def pseudobulk_plus_plus(data = None, adataObj = None, cluster = None, label = None,numFeat = None):
+#### Pseudobulk++
+def pseudobulk_plus_plus(data = None, adataObj = None, cluster = None, label = None,numFeat = None, label_type='categorical'):
     """
     Implements with fixed importance per gene
 
@@ -41,72 +43,40 @@ def pseudobulk_plus_plus(data = None, adataObj = None, cluster = None, label = N
         sample-level features
     samp_label: sample level labels
     """
-
-
-    #first part is the ranking genes by overall importance
-    adata_merge.uns['log1p']["base"] = None
+    adataObj.uns['log1p']["base"] = None
     sc.tl.rank_genes_groups(adataObj, groupby="leiden", use_raw=False)
-    dedf = sc.get.rank_genes_groups_df(adata_merge, group = None)
-    print(dedf)
-    dedf.iloc[:,2] = dedf.iloc[:,2].abs()
+    dedf = sc.get.rank_genes_groups_df(adataObj, group=None)
+    
+    dedf.iloc[:, 2] = dedf.iloc[:, 2].abs()
     dedf['scores'] = dedf['scores'].map(lambda x: math.log(x))
-
-    temp = dedf.sort_values(by = 'scores', ascending = False)
-    temp = temp.iloc[:numFeat]
-    dedf = temp
-    #print(dedf)
-    dedf = dedf[['names','scores']]
-    print(dedf)
+    dedf = dedf.sort_values(by='scores', ascending=False).iloc[:numFeat][['names', 'scores']]
 
     row_ind = 0
     col_ind = 0
 
-    pb_feat_mat = np.zeros((data['sample_id'].nunique(),data[cluster].nunique()))
-    #pb_feat_mat = np.zeros((data['sample_id'].nunique(),2)) #testing
-
+    pb_feat_mat = np.zeros((data['sample_id'].nunique(), data[cluster].nunique()))
     samp_label = []
 
     for samp in data['sample_id'].unique():
 
-        print(samp)
-
         for_label = data[((data['sample_id'] == samp))]
-
-        samp_label.append(for_label['age'].value_counts().idxmax())
-
-        print(for_label['age'].value_counts().idxmax())
-
+        if label_type == 'categorical':
+            samp_label.append(for_label[label].value_counts().idxmax())
+        else:  # continuous
+            samp_label.append(for_label[label].astype(float).mean())
         samp_feat_vec = []
-
         for cl in data[cluster].unique():
-            print(cl)
-
             #select by sample and cluster
             sample_by_cl = data[((data['sample_id'] == samp) & (data[cluster] == cl))]
-
             if sample_by_cl.empty:
                 sum_wa = 0
-                print('is empty!')
-
             else:
-                print('not empty')
-
-                #toy example for testing
-                #df_de_dict = {'gene_name':['IL1B','APOE'],'importance':[2,3]}
-                #df_de = pd.DataFrame.from_dict(df_de_dict)
-
                 feature_only = sample_by_cl[dedf['names']].to_numpy()
                 importance_vec = dedf['scores'].to_numpy()
-
                 weighted_avg = feature_only @ importance_vec
-
                 sum_wa = weighted_avg.sum()
-
             samp_feat_vec += [sum_wa]
-
-            print(samp_feat_vec)
 
         pb_feat_mat[row_ind,:] = samp_feat_vec
         row_ind = row_ind + 1
-
     return pb_feat_mat, samp_label
